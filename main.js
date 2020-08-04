91ded108b11e3412431dd48a4c94ba6400772f5a
svalenzuela <59404624+svalenzuela1@users.noreply.github.com>
2020-08-04 14:18:49 -0400

    backend content shows up

--- main.js
+++ main.js
@@ -3,10 +3,8 @@
 //const {productModel, cartModel} = require("../backend-api/models/schemas.js")
 
 
-const $gameImages = $('<div>').addClass('gameImages')
+//const $gameImages = $('<div>').addClass('gameImages')
 const URL = 'https://project2-backend-api.herokuapp.com'
-
-
 fetch( URL + '/')
 .then(response => response.json())
 .then(data => {
@@ -15,10 +13,9 @@ fetch( URL + '/')
 
 
 
-//global variables 
 
 //functions
-const getGameImages = async () => {
+const getGames = async () => {
     const response = await fetch('http://localhost:3000/')
     const {product, cart} = await response.json()
     console.log(product, cart)
@@ -26,17 +23,48 @@ const getGameImages = async () => {
 }
 
 //main app logic
-getGameImages()
+getGames()
 
+//show image names
 const showGames = (games) => {
     console.log(games)
     games.forEach(game => {
         console.log(game)
-        const $ul = $('<ul>').html(`
+        const $content = $('<div>').html(`
         <h1>${game.name}</h1>
+        <h3>${game.description}</h3>
+        <img src=${game.imageURL}>
+        <h5> ${game.price}</h6>
     `)
+       // $(content).css("background-color","purple")
+        $('body').append($content)
+})}
 
-        $('body').append($ul)
-})
+//CSS for $content
+//$(content).css("display","flex")
+//$(content).css("text-align","center")
 
+
+//HAMBURGER MENU
+const $burger = $(".burger")
+const $link = $(".link")
+let show = false;
+
+const showMenu = (event) => {
+    if (show) {
+        $link.each(function(index){
+            $(this).css("display","none")
+        })
+        show = false
+    } else {
+        $link.each(function(index){
+            $(this).css("display","block")
+        })
+        show = true
+    }
 }
+
+$burger.on("click", showMenu)
+//Hamburger menu code ends here
+ 
+
