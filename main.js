9cdf9c7c97aa001c47316f216851e9330cfd663e
svalenzuela <59404624+svalenzuela1@users.noreply.github.com>
2020-08-07 04:39:36 -0400

    media queries added

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-//const {productModel, cartModel} = require("../backend-api/models/schemas.js")
 
-
-//const $gameImages = $('<div>').addClass('gameImages')
-const URL = 'http://localhost:3000'//'https://project2-backend-api.herokuapp.com'
+const URL = 'http://localhost:3000' //'https://project2-backend-api.herokuapp.com'
 
 fetch( URL + '/')
 .then(response => response.json())
 .then(data => {
-    console.log(data)
 })
 
 
@@ -26,24 +22,20 @@ const getGames = async () => {
 //main app logic
 getGames()
 
-//PUT THIS BACK IN CONTENT IF ALL ELSE FAILS
-//<h3>${game.description}</h3>
-// <img src=${game.imageURL}>
-//</img>/ <h5> ${game.price}</h5>
 
 
 //show image names
 const showGames = (games) => {
-    console.log(games)
+   
     games.forEach(game => {
         console.log(game)
         const $gameHeader = $('<div>').html(`
         <h1>${game.name}</h1>`).addClass('gameHeader')
-       // $(content).css("background-color","purple")
+       
         $('.games').append($gameHeader)
         
         const $gameDescription = $('<div>').html(`
-        <h3>${game.description}</h3>`).addClass('gameDescription')
+        <h3><i>${game.description}</i></h3>`).addClass('gameDescription')
         $('.games').append($gameDescription)
 
         const $gameImage = $('<div>').html(`
@@ -76,8 +68,6 @@ if(!$('button.cart').attr('id')){
 
 
 
-    console.log(event.target.id)
-
     const body = {
         productToAdd: event.target.id
 }
@@ -90,7 +80,7 @@ const response = await fetch(URL + '/carts/' +  $('button.cart').attr('id'), {
 
 }).then(res => res.json())
 
-console.log(response)
+
 showCart(response.product)
 }
 
@@ -99,21 +89,18 @@ showCart(response.product)
 
 
 const showCart = (items) => {
-    console.log(items)
+    
     $('#requestCart').empty() 
     items.forEach(item => {
-        console.log(item)
+       
            
-       for(let i = 0; i < 1; i++){
            //heading for cart
     const $itemHeading = $('<h1>').html(`
     <h1>Added To Cart</h1>`).addClass('itemHeading')
     $('#requestCart').append($itemHeading)
     $($itemHeading).css("text-align","center")
     $($itemHeading).css("color","azure")
-    //$($itemHeading).css("background-color","red")
-        //return 1
-       }
+    
 
 
         //cart items below
@@ -121,11 +108,13 @@ const showCart = (items) => {
         <h1>${item.name}</h1>`).addClass('itemName')
         $($itemName).css("display","flex")
         $($itemName).css("justify-content","center")
+        $($itemName).css("text-align","center")
 
         const $itemDescription = $('<div>').html(`
         <h1><i>${item.description}</i></h1>`).addClass('itemDescription')
         $($itemDescription).css("display","flex")
         $($itemDescription).css("justify-content","center")
+        $($itemDescription).css("text-align","center")
 
         const $itemImage = $('<div>').html(`
         <img src=${item.imageURL}>`).addClass('itemImage')
@@ -145,23 +134,6 @@ const showCart = (items) => {
     })
 }
 
-
-
-//TRYING TO TARGET ID 
-
-//const catchProduct = async (response) => {
-  //  const response = await fetch(URL + '/carts')
-//}
-
-
-
-
-
-
-
-//CSS for $content
-//$(content).css("display","flex")
-//$(content).css("text-align","center")
 
 
 //HAMBURGER MENU
